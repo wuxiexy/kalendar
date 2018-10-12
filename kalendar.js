0a3ed1f85c0b04c7b815c0070bf1c13d1d4d1da5
wuxiexy <wuxiexy@sina.cn>
2018-10-12 16:57:51 +0800

    test

---
+++ kalendar.js
@@ -0,0 +1,50 @@
+/*
+* 日历插件
+* author wx
+*
+* */
+
+/*;(function (w, doc) {
+
+}(window, document));*/
+
+
+// param: el 元素的 id
+function Kalendar(el) {
+
+    var doc = document
+        , win = window
+        , i
+        , len
+        , current
+        , container = doc.getElementById('Kalendar-popop')
+        , ele = doc.getElementById(el)
+    ;
+
+    
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
